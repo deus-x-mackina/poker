f12cfcad7d7ddf1b3527d86d273a1b4d7f2d9561
Daniel Mack <drmterps@yahoo.com>
2021-04-23 21:07:30 -0400

    Further optimize benches

--- benches/evaluation.rs
+++ benches/evaluation.rs
@@ -7,24 +7,35 @@ fn bench_evaluator(c: &mut Criterion) {
 }
 
 fn bench_dynamic_eval(c: &mut Criterion) {
+    let mut group = c.benchmark_group("dynamic-eval");
+    group.sample_size(10);
     let eval = Evaluator::new();
     let gen = Card::generate_deck().combinations(5).collect::<Box<_>>();
-    c.bench_function("Evaluator evaulation", |b| {
+    group.bench_function("Evaluator evaulation", |b| {
         b.iter(|| for cards in gen.iter() {
             eval.evaluate(cards).unwrap();
         })
     });
+    group.finish();
 }
 
 fn bench_static_eval(c: &mut Criterion) {
+    let mut group = c.benchmark_group("static-eval");
+    group.sample_size(10);
     let gen = Card::generate_deck().combinations(5).collect::<Box<_>>();
-    c.bench_function("Static evaluation", |b| {
+    group.bench_function("Static evaluation", |b| {
         b.iter(|| for cards in gen.iter() {
             static_lookup::evaluate(cards).unwrap();
         });
     });
+    group.finish();
 }
 
-criterion_group!(benches, bench_evaluator, bench_dynamic_eval, bench_static_eval);
+criterion_group!(
+    benches,
+    bench_evaluator,
+    bench_dynamic_eval,
+    bench_static_eval
+);
 
 criterion_main!(benches);
