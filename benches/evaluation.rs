0669ce0e9a91de1ea69aaac061f263b761259647
Daniel Mack <drmterps@yahoo.com>
2021-05-11 09:39:39 -0400

    Simplify benches

--- benches/evaluation.rs
+++ benches/evaluation.rs
@@ -1,41 +1,46 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use poker::{Evaluator, evaluate::static_lookup, Card};
+use criterion::{Criterion, criterion_group, criterion_main};
 use itertools::Itertools;
+use poker::{evaluate::static_lookup, Card, Evaluator};
 
 fn bench_evaluator(c: &mut Criterion) {
-    c.bench_function("Evaluator::new()", |b| b.iter(black_box(Evaluator::new)));
+    c.bench_function("Evaluator::new()", |b| b.iter(Evaluator::new));
 }
 
-fn bench_dynamic_eval(c: &mut Criterion) {
-    let mut group = c.benchmark_group("dynamic-eval");
+fn bench_eval(c: &mut Criterion) {
+    let mut group = c.benchmark_group("eval");
     group.sample_size(10);
     let eval = Evaluator::new();
     let gen = Card::generate_deck().combinations(5).collect::<Box<_>>();
-    group.bench_function("Evaluator evaulation", |b| {
-        b.iter(|| for cards in gen.iter() {
-            eval.evaluate(cards).unwrap();
-        })
-    });
-    group.finish();
-}
 
-fn bench_static_eval(c: &mut Criterion) {
-    let mut group = c.benchmark_group("static-eval");
-    group.sample_size(10);
-    let gen = Card::generate_deck().combinations(5).collect::<Box<_>>();
-    group.bench_function("Static evaluation", |b| {
-        b.iter(|| for cards in gen.iter() {
-            static_lookup::evaluate(cards).unwrap();
+    let routine = {
+        let eval = eval;
+        let gen = &gen;
+        move || {
+            for cards in gen.iter() {
+                let _ = eval.evaluate(cards);
+            }
+        }
+    };
+
+    group.bench_function("dynamic", |b| {
+        b.iter(&routine)
+    });
+
+    group.bench_function("static", |b| {
+        b.iter(|| {
+            for cards in gen.iter() {
+                let _ = static_lookup::evaluate(cards);
+            }
         });
     });
+
     group.finish();
 }
 
 criterion_group!(
     benches,
     bench_evaluator,
-    bench_dynamic_eval,
-    bench_static_eval
+    bench_eval
 );
 
 criterion_main!(benches);
