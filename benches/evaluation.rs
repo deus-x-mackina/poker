4288d70c24bd965e58da52767f212ce788cf9719
Daniel Mack <>
2023-07-19 08:00:48 -0400

    Add benches for single hand evaluation

--- benches/evaluation.rs
+++ benches/evaluation.rs
@@ -1,9 +1,53 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use itertools::Itertools;
-use poker::{evaluate::static_lookup, Card, Evaluator};
+use poker::{cards, evaluate::static_lookup, Card, Evaluator};
 
 fn bench_evaluator(c: &mut Criterion) {
     c.bench_function("Evaluator::new()", |b| b.iter(Evaluator::new));
+}
+
+fn bench_single_5card_hand_eval(c: &mut Criterion) {
+    let mut group = c.benchmark_group("single_5card_hand_eval");
+
+    let eval = Evaluator::new();
+    let hand: Vec<_> = cards!("Th", "Jh", "Qh", "Kh", "Ah").try_collect().unwrap();
+
+    group.bench_function("dynamic", |b| {
+        b.iter(|| {
+            let _ = eval.evaluate(&hand);
+        })
+    });
+
+    group.bench_function("static", |b| {
+        b.iter(|| {
+            let _ = static_lookup::evaluate(&hand);
+        })
+    });
+    
+    group.finish();
+}
+
+fn bench_single_7card_hand_eval(c: &mut Criterion) {
+    let mut group = c.benchmark_group("single_7card_hand_eval");
+
+    let eval = Evaluator::new();
+    let hand: Vec<_> = cards!("8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah")
+        .try_collect()
+        .unwrap();
+
+    group.bench_function("dynamic", |b| {
+        b.iter(|| {
+            let _ = eval.evaluate(&hand);
+        })
+    });
+
+    group.bench_function("static", |b| {
+        b.iter(|| {
+            let _ = static_lookup::evaluate(&hand);
+        })
+    });
+    
+    group.finish();
 }
 
 fn bench_eval(c: &mut Criterion) {
@@ -35,6 +79,12 @@ fn bench_eval(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_evaluator, bench_eval);
+criterion_group!(
+    benches,
+    bench_evaluator,
+    bench_single_5card_hand_eval,
+    bench_single_7card_hand_eval,
+    bench_eval
+);
 
 criterion_main!(benches);
